17c33be850836515c3ca4dfac03b5e34c2faa346
sethripman <sethripman@gmail.com>
2019-11-12 11:54:24 -0800

    wrote addtodo

--- public/services/todo-api.js
+++ public/services/todo-api.js
@@ -45,4 +45,3 @@ export function removeTodo(todoId) {
         method: 'DELETE'
     });
 }
-
--- public/todo/AddTodo.js
+++ public/todo/AddTodo.js
@@ -2,30 +2,36 @@ import Component from '../Component.js';
 
 class AddTodo extends Component {
 
-    onRender(form) {
+    onRender(dom) {
         const onAdd = this.props.onAdd;
-        
+        const form = dom.querySelector('form');
+        const input = dom.querySelector('input[name=todo]');
         form.addEventListener('submit', async event => {
             event.preventDefault();
 
+            const todo = {
+                task: input.value
+            };
+
             try {
                 await onAdd(todo);
-                // this only runs if no error:
                 form.reset();
                 document.activeElement.blur();
             }
             catch (err) {
-                // nothing to do as App will show error,
-                // but will keep form from clearing...
+                // butts
             }
         });
     }
 
     renderHTML() {
         return /*html*/`
-            <form>
-                
-            </form>
+            <section class="todo-form-section">
+                <form class="todo-form">
+                    <input name="todo" required>
+                    <button>Add</button>
+                </form>
+            </section>
         `;
     }
 }
